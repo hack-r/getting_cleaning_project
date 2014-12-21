b5ac7daa3dcf7a166f70aa73c05bbc8f667f278a
Jason Miller <root@hack-r.com>
2014-12-21 14:36:43 -0500

    minor changes, no analysis yet present

--- CodeBook.md
+++ CodeBook.md
@@ -5,13 +5,6 @@ date: "Sunday, December 21, 2014"
 output: html_document
 ---
 
-# CookBook.md Table of Contents
-## 1. Data
-### 1.1 Link to External Description of the Data Set
-### 1.2 Link to the Raw Data
-## 2. Study Design
-## 3. Code Book
-
 # 1. Data
 This section provides a high-level overview of the dataset, as well as a link to the original dataset and its description.
 
@@ -22,6 +15,7 @@ http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartpho
 
 ## 1.2 Link to the Raw Data
 A compressed archive of the original dataset is available at the following URI:
+
 https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
 ## 2. Study Design
--- run_analysis.R
+++ run_analysis.R
@@ -7,3 +7,15 @@
 # Uses descriptive activity names to name the activities in the data set
 # Appropriately labels the data set with descriptive variable names. 
 # From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+# Set Options -------------------------------------------------------------
+setwd("C://Users//Jason//Dropbox//Coursera//getting_and_cleaning//getting_cleaning_project")
+
+# Load Libraries/Functions ------------------------------------------------
+library()
+library()
+library()
+
+# Import Raw Data ---------------------------------------------------------
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile = "data.zip")
